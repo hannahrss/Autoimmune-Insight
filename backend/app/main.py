2b0ad4fbb401d1cc6421f72b6a02b7309400d0e0
Hannah Scaglione <hscaglione9575@gmail.com>
2026-04-08 22:49:07 -0400

    Specifying user inputs and adding validating conditions

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,32 @@
-from fastapi import FastAPI, Path, HTTPException, Query #library and important parameters
-from pydantic import BaseModel, Field, computed_field #for data validation
-from pydantic import Annotated, Literal, Optional #used to add descriptions
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI #library and important parameters
+from pydantic import BaseModel, field_validator #for data validation
+from typing import Optional #used to add descriptions
 import datetime
-import json
 
 class UserSymptom(BaseModel):
-    date: Annotated[Optional[datetime.date]]
+    date: Optional[datetime.date]
     symptom: str | None = None
     severity: int
-    description: str | None = None
+    sleep_hours: float
+    energy_level: int
+
+    @field_validator('severity')
+    @classmethod
+    def severity_must_be_valid(cls, v):
+        if not 1 <= v <= 10:
+            raise ValueError('Severity must be on scale between 1 and 10')
+        return v
+    
+    @field_validator(sleep_hours)
+    @classmethod
+    def sleep_must_be_valid(cls, s):
+        if not 1 <= s <= 24:
+            raise ValueError('Hours of sleep must be less than a day')
+        return s
+    
+
+
+
 
 app = FastAPI()
 symptoms = []
@@ -22,11 +39,15 @@ def hello():
 # creating an endpoint as "view" where all the data shows up
 # when a user accesses https://{url}/view API will display records
 
-@app.put("/symptom", response_model=UserSymptom)
+# placing food order; can update model; automatic JSON parsing/validation
+@app.post("/symptom", response_model=UserSymptom) 
 async def track_symptom(symptom: UserSymptom):
     symptoms.append(symptom)
     return symptom
 
+# decorator defines endpoint (asking to see menu)
+# can return HTTP exceptions 
+# (e.g. warning that item doesn't exist, success placing item)
 @app.get("/symptom", response_model=list[UserSymptom])
 async def get_all_symptoms():
     return symptoms
